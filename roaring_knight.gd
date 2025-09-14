ad6f084e3932eca4e18344087eda24fcf51d2e6e
KyuDeathsBR_ <72417577+KyuDeathsBR@users.noreply.github.com>
2025-09-14 19:11:24 -0300

    AAAAAAAAAAAAA

--- roaring_knight.gd
+++ roaring_knight.gd
@@ -10,13 +10,13 @@ var time : float = 0;
 func _process(dt : float):
 	time_off += dt;
 	time += dt;
-	origin_sprite.position.y += sin(time*4)
+	self.position.y += sign(sin(time))/8.0
 	
-	var frame : int = origin_sprite.frame;
-	var anim : StringName = origin_sprite.animation;
-	if (frame != prev_frame || anim != prev_anim) || time_off > delta:
-		spawn_phantom(anim,frame);
-		time_off = 0.0;
+	#var frame : int = origin_sprite.frame;
+	#var anim : StringName = origin_sprite.animation;
+	#if (frame != prev_frame || anim != prev_anim) || time_off > delta:
+	#	spawn_phantom(anim,frame);
+	#	time_off = 0.0;
 		
 func destroy_clone(clone : AnimatedSprite2D):
 	clone.queue_free()
@@ -27,13 +27,9 @@ func spawn_phantom(animation : StringName, frame : int):
 	clone.play(animation)
 	clone.original = false;
 	clone.frame = max(frame-1,0);
-	var tween = create_tween()
 	var count = clone.sprite_frames.get_frame_count(animation)
 	prev_frame = frame;
 	prev_anim = animation;
 	clone.modulate.a = 1.0;
 	var duration : float = min((float)(count-frame)/frame,delta*2)
-	tween.tween_property(clone, "position:x", clone.position.x + 22.5, duration*2).set_ease(Tween.EASE_OUT)
-	tween.tween_callback(clone.queue_free)
-	tween.play()
 	clone.z_index = origin_sprite.z_index - 1;
--- tween_if_clone.gd
+++ tween_if_clone.gd
@@ -1,8 +1,12 @@
 extends AnimatedSprite2D
 @export var original : bool = true
 var duration = 0.2;
+var t = 0.0;
 func _ready() -> void:
 	self.play()
 func _process(delta: float) -> void: 
+	t += delta;
 	if !original:
 		self.modulate.a = clamp(self.modulate.a-delta/duration,0,1)
+		if !roundi(t*48)%3:
+			self.position.x += 4
